fdb3c54413bbadfa380beb7585e5e4bad6318494
wivest <120594169+wivest@users.noreply.github.com>
2024-05-06 19:58:08 +0200

    feat: create Factory method pattern

---
+++ Creational/FactoryMethod.cs
@@ -0,0 +1,33 @@
+namespace DesignPatterns.Creational.FactoryMethod
+{
+    // abstract declaration
+
+    interface IItem { }
+
+    abstract class Factory
+    {
+        public abstract IItem Create();
+    }
+
+    // abstract implementation
+
+    class ItemA : IItem { }
+
+    class ItemB : IItem { }
+
+    class FactoryA : Factory
+    {
+        public override IItem Create()
+        {
+            return new ItemA();
+        }
+    }
+
+    class FactoryB : Factory
+    {
+        public override IItem Create()
+        {
+            return new ItemB();
+        }
+    }
+}

7a80da3a8e78f2e24d055028b356c97f1a4f0e37
wivest <120594169+wivest@users.noreply.github.com>
2024-05-08 18:35:38 +0200

    feat: create Builder pattern

---
+++ Creational/Builder.cs
@@ -0,0 +1,65 @@
+namespace DesignPatterns.Creational.Builder
+{
+    // interface declaration
+
+    interface IBuilder
+    {
+        public void Reset();
+
+        public void SetParameterA();
+
+        public void SetParameterB();
+    }
+
+    // implementation
+
+    class Builder : IBuilder
+    {
+        private Product _product = new Product();
+
+        public void Reset()
+        {
+            _product = new Product();
+        }
+
+        public void SetParameterA() { }
+
+        public void SetParameterB() { }
+
+        public Product GetResult()
+        {
+            return _product;
+        }
+    }
+
+    // class declaration
+
+    class Product { }
+
+    // optional
+    class Director
+    {
+        public void MakeProduct(IBuilder builder)
+        {
+            builder.Reset();
+            builder.SetParameterA();
+            builder.SetParameterB();
+        }
+    }
+
+    // access director and builder
+    class Program
+    {
+        static void Main()
+        {
+            Director director = new Director();
+
+            // type is not an interface
+            // otherwise GetResult is unavailable
+            Builder builder = new Builder();
+
+            director.MakeProduct(builder);
+            Product product = builder.GetResult();
+        }
+    }
+}

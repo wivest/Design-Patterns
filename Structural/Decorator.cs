1b097739e0a22a344fca8324181fc135b6297e81
wivest <120594169+wivest@users.noreply.github.com>
2024-05-11 12:02:08 +0200

    feat: create Decorator pattern

---
+++ Structural/Decorator.cs
@@ -0,0 +1,54 @@
+namespace DesignPatterns.Structural.Decorator
+{
+    // interface declaration
+
+    interface IComponent
+    {
+        public void Method();
+    }
+
+    // implementation
+
+    class Component : IComponent
+    {
+        public void Method() { }
+    }
+
+    class Decorator : IComponent
+    {
+        private IComponent _component;
+
+        public Decorator(IComponent component)
+        {
+            _component = component;
+        }
+
+        public void Method()
+        {
+            _component.Method();
+        }
+    }
+
+    class DerivedDecorator : Decorator
+    {
+        public DerivedDecorator(IComponent component)
+            : base(component) { }
+
+        public new void Method()
+        {
+            base.Method();
+            // extra actions
+        }
+    }
+
+    // class declaration
+
+    class Program
+    {
+        public static void Main()
+        {
+            IComponent component = new Component();
+            component = new DerivedDecorator(component);
+        }
+    }
+}

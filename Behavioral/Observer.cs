954e25cde34afe7441abb8f639dbcf998dcf1bd6
wivest <120594169+wivest@users.noreply.github.com>
2024-05-18 18:06:37 +0200

    feat: create Observer pattern

---
+++ Behavioral/Observer.cs
@@ -0,0 +1,44 @@
+namespace DesignPatterns.Behavioral.Observer
+{
+    // interface declaration
+
+    interface ISubscriber
+    {
+        public void Update(Context context);
+    }
+
+    // class declaration
+
+    class Publisher
+    {
+        private List<ISubscriber> _subscribers = new List<ISubscriber>();
+        private Context _context = new Context();
+
+        public void Subscribe(ISubscriber subscriber)
+        {
+            if (_subscribers.Contains(subscriber))
+                return;
+            _subscribers.Add(subscriber);
+        }
+
+        public void Unsubscribe(ISubscriber subscriber)
+        {
+            _subscribers.Remove(subscriber);
+        }
+
+        private void Notify()
+        {
+            foreach (var s in _subscribers)
+            {
+                s.Update(_context);
+            }
+        }
+
+        public void Method()
+        {
+            Notify();
+        }
+    }
+
+    class Context { }
+}

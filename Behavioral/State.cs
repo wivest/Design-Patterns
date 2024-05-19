a47abf3f3b517fa7927c40447b9bf373a8fef55a
wivest <120594169+wivest@users.noreply.github.com>
2024-05-19 17:06:55 +0200

    feat: create State pattern

---
+++ Behavioral/State.cs
@@ -0,0 +1,55 @@
+namespace DesignPatterns.Behavioral.State
+{
+    // interface declaration
+
+    interface IState
+    {
+        public Context Context { set; }
+
+        public void Method();
+    }
+
+    // abstract class declaration
+
+    abstract class State : IState
+    {
+        public Context? Context { protected get; set; }
+
+        public abstract void Method();
+    }
+
+    // implementation
+
+    class State1 : State
+    {
+        public override void Method()
+        {
+            Context?.SetState(new State2());
+        }
+    }
+
+    class State2 : State
+    {
+        public override void Method()
+        {
+            Context?.SetState(new State1());
+        }
+    }
+
+    // class declaration
+
+    class Context
+    {
+        private IState _state;
+
+        public Context(IState state)
+        {
+            _state = state;
+        }
+
+        public void SetState(IState state)
+        {
+            _state = state;
+        }
+    }
+}
